2d1b6eccc55ba49bff7f95a7e7fbc1dc5ee19505
Emre Dogan <48212096+EmreDogann@users.noreply.github.com>
2026-01-19 03:32:43 +0000

    Cleaned up GuidComponentDrawer.cs

--- Packages/com.EmreeDev.UnityGuidReference/Editor/GuidComponentDrawer.cs
+++ Packages/com.EmreeDev.UnityGuidReference/Editor/GuidComponentDrawer.cs
@@ -56,45 +56,6 @@ public class GuidComponentDrawer : Editor
     // SerializedProperty here only used for remembering the state of foldout:
     // https://discussions.unity.com/t/editorguilayout-foldout-no-way-to-remember-state/36422/6
     private SerializedProperty _serializedGuidProp;
-    private bool _isInit;
-
-    private static ProfilerMarker _profileMarker =
-        new ProfilerMarker($"{nameof(GuidComponentDrawer)}.{nameof(OnInspectorGUI)}");
-    private static ProfilerMarker _profileMarker2 = new ProfilerMarker($"{nameof(GuidComponentDrawer)}.OnTitlebarGUI");
-    private static readonly Type Type = typeof(EditorWindow).Assembly.GetType("UnityEditor.InspectorWindow");
-    private static readonly Type Type2 = typeof(EditorWindow).Assembly.GetType("UnityEditor.PropertyEditor");
-    private static readonly Type Type3 = typeof(EditorWindow).Assembly.GetType("UnityEditor.UIElements.EditorElement");
-    private static readonly FieldInfo Field =
-        Type2.GetField("m_EditorsElement", BindingFlags.NonPublic | BindingFlags.Instance);
-    private static readonly FieldInfo Field2 =
-        Type3.GetField("m_Header", BindingFlags.NonPublic | BindingFlags.Instance);
-    private static readonly FieldInfo Field3 =
-        Type3.GetField("m_EditorTarget", BindingFlags.NonPublic | BindingFlags.Instance);
-    private static VisualElement m_EditorsElement;
-    private static VisualElement EditorsElement => m_EditorsElement ??= GetEditorVisualElement();
-    private readonly Dictionary<VisualElement, Action> Callbacks = new Dictionary<VisualElement, Action>();
-
-    private static VisualElement GetEditorVisualElement()
-    {
-        EditorWindow window = EditorWindow.GetWindow(Type);
-        if (window)
-        {
-            return Field.GetValue(window) as VisualElement;
-        }
-
-        return null;
-    }
-
-    private void Initialize()
-    {
-        if (!_isInit)
-        {
-            _guidComp = (GuidComponent)serializedObject.targetObject;
-            FindHeaderGUI();
-
-            _isInit = true;
-        }
-    }
 
     public override VisualElement CreateInspectorGUI()
     {
@@ -114,7 +75,6 @@ public class GuidComponentDrawer : Editor
         else
         {
             CustomInspectorField inspectorFieldGOGuid = new CustomInspectorField("GameObject GUID");
-
             Label goLabelValue = new Label(_guidComp.GetGuid().ToString())
             {
                 style = { flexGrow = 1 }
@@ -141,8 +101,6 @@ public class GuidComponentDrawer : Editor
                 // Persist expansion state
                 foldout.RegisterValueChangedCallback(evt =>
                 {
-                    // _serializedGuidProp.isExpanded = evt.newValue;
-
                     foldout.contentContainer.Clear();
                     if (evt.newValue)
                     {
@@ -181,48 +139,32 @@ public class GuidComponentDrawer : Editor
         }
     }
 
-    public override void OnInspectorGUI()
+    // ---- Header GUI ----
+    private static ProfilerMarker _profileMarker =
+        new ProfilerMarker($"{nameof(GuidComponentDrawer)}.{nameof(OnInspectorGUI)}");
+    private static ProfilerMarker _profileMarker2 = new ProfilerMarker($"{nameof(GuidComponentDrawer)}.OnTitlebarGUI");
+    private static readonly Type Type = typeof(EditorWindow).Assembly.GetType("UnityEditor.InspectorWindow");
+    private static readonly Type Type2 = typeof(EditorWindow).Assembly.GetType("UnityEditor.PropertyEditor");
+    private static readonly Type Type3 = typeof(EditorWindow).Assembly.GetType("UnityEditor.UIElements.EditorElement");
+    private static readonly FieldInfo Field =
+        Type2.GetField("m_EditorsElement", BindingFlags.NonPublic | BindingFlags.Instance);
+    private static readonly FieldInfo Field2 =
+        Type3.GetField("m_Header", BindingFlags.NonPublic | BindingFlags.Instance);
+    private static readonly FieldInfo Field3 =
+        Type3.GetField("m_EditorTarget", BindingFlags.NonPublic | BindingFlags.Instance);
+    private static VisualElement m_EditorsElement;
+    private static VisualElement EditorsElement => m_EditorsElement ??= GetEditorVisualElement();
+    private readonly Dictionary<VisualElement, Action> _callbacks = new Dictionary<VisualElement, Action>();
+
+    private static VisualElement GetEditorVisualElement()
     {
-        serializedObject.Update();
-
-        _serializedGuidProp = serializedObject.FindProperty("_guid");
-        Initialize();
-
-        if (_guidComp.IsAssetOnDisk())
+        EditorWindow window = EditorWindow.GetWindow(Type);
+        if (window)
         {
-            EditorGUILayout.HelpBox(
-                "Guid Components do not work in prefab assets.\nHowever, instances of prefabs in a scene will receive the correct guid information.",
-                MessageType.Warning);
-        }
-        else
-        {
-            // Draw label
-            EditorGUILayout.LabelField("GameObject GUID", _guidComp.GetGuid().ToString());
-
-            if (_guidComp.GetComponentGUIDs().Count > 0)
-            {
-                _serializedGuidProp.isExpanded =
-                    EditorGUILayout.Foldout(_serializedGuidProp.isExpanded, "Component GUIDs", true);
-                if (_serializedGuidProp.isExpanded)
-                {
-                    EditorGUI.indentLevel++;
-                    foreach (ComponentGuid componentGuid in _guidComp.GetComponentGUIDs())
-                    {
-                        string label = "";
-#if COMPONENT_NAMES
-                        label = $"{componentGuid.cachedComponent.GetName()}:";
-#else
-                        label = $"{componentGuid.cachedComponent.GetType().Name}:";
-#endif
-                        EditorGUILayout.LabelField(label, componentGuid.Guid.ToString());
-                    }
-
-                    EditorGUI.indentLevel--;
-                }
-            }
+            return Field.GetValue(window) as VisualElement;
         }
 
-        serializedObject.ApplyModifiedProperties();
+        return null;
     }
 
     private void FindHeaderGUI()
@@ -244,40 +186,38 @@ public class GuidComponentDrawer : Editor
                 }
 
                 Object localTarget = Field3.GetValue(element) as Object;
-                if (localTarget is GuidComponent)
+                if (localTarget is not GuidComponent)
                 {
-                    IMGUIContainer value2 = Field2.GetValue(element) as IMGUIContainer;
-                    Action callback = null;
-                    if (Callbacks.TryGetValue(element, out Action found))
-                    {
-                        callback = found;
-                    }
-                    else
-                    {
-                        callback = Callbacks[element] = MyLocalCallback;
-                    }
+                    continue;
+                }
 
-                    if (value2 != null)
-                    {
-                        value2.onGUIHandler = callback;
-                    }
+                IMGUIContainer value2 = Field2.GetValue(element) as IMGUIContainer;
+                Action callback = null;
+                if (_callbacks.TryGetValue(element, out Action found))
+                {
+                    callback = found;
+                }
+                else
+                {
+                    callback = _callbacks[element] = OnHeaderGUICallback;
+                }
 
-                    void MyLocalCallback()
+                if (value2 != null)
+                {
+                    value2.onGUIHandler = callback;
+                }
+
+                void OnHeaderGUICallback()
+                {
+                    using (_profileMarker2.Auto())
                     {
-                        using (_profileMarker2.Auto())
+                        try
                         {
-                            try
-                            {
-                                // OnHeaderGUI();
-                                // Editor guidComponentDrawer = this;
-                                // DrawFoldoutInspector(_guidComp, ref guidComponentDrawer);
-                                EditorGUILayout.InspectorTitlebar(true, _guidComp, true);
-                                // OnTitlebarGUI?.Invoke(GUILayoutUtility.GetLastRect(), localTarget);
-                            }
-                            catch (Exception e)
-                            {
-                                Debug.LogException(e);
-                            }
+                            EditorGUILayout.InspectorTitlebar(true, _guidComp, true);
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogException(e);
                         }
                     }
                 }
