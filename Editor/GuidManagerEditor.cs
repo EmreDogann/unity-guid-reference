80a9739491766ad4dc23a079ba2734399e3259be
Emre Dogan <48212096+EmreDogann@users.noreply.github.com>
2026-03-26 01:00:04 +0000

    Cleanup unused code & Debug.Log() statements.

--- Editor/GuidManagerEditor.cs
+++ Editor/GuidManagerEditor.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 [InitializeOnLoad]
 // Class to handle registering and accessing objects by GUID
 public class GuidManagerEditor
 {
-    // Instance data
-    private static PlayModeStateChange _playModeStateChange;
-
     private static GuidMappings GetMappings()
     {
         return GuidMappings.Instance;
@@ -133,10 +129,6 @@ public class GuidManagerEditor
 
         GuidComponent.OnGuidRemoved -= OnComponentRemoved;
         GuidComponent.OnGuidRemoved += OnComponentRemoved;
-
-        // ObjectChangeEvents.changesPublished -= ChangesPublished;
-        // ObjectChangeEvents.changesPublished += ChangesPublished;
-        // ObjectFactory.componentWasAdded += OnComponentAdded;
     }
 
     private static void OnComponentRemoved(ComponentGuid guid)
@@ -150,147 +142,6 @@ public class GuidManagerEditor
         {
             // Component is destroyed at this point, cannot retrieve its GlobalObjectID, so we look up by guid.
             GetMappings().OrphanGuid(guid.GlobalGameObjectId, item);
-        }
-    }
-
-    private static void ChangesPublished(ref ObjectChangeEventStream stream)
-    {
-        for (int i = 0; i < stream.length; ++i)
-        {
-            ObjectChangeKind type = stream.GetEventType(i);
-            switch (type)
-            {
-                case ObjectChangeKind.None:
-                    break;
-                case ObjectChangeKind.ChangeScene:
-                    stream.GetChangeSceneEvent(i, out ChangeSceneEventArgs changeSceneEvent);
-                    Debug.Log($"{type}: {changeSceneEvent.scene}");
-                    break;
-                case ObjectChangeKind.CreateGameObjectHierarchy:
-                    stream.GetCreateGameObjectHierarchyEvent(i,
-                        out CreateGameObjectHierarchyEventArgs createGameObjectHierarchyEvent);
-                    GameObject newGameObject =
-                        EditorUtility.EntityIdToObject(createGameObjectHierarchyEvent.instanceId) as GameObject;
-                    Debug.Log($"{type}: {newGameObject} in scene {createGameObjectHierarchyEvent.scene}.");
-                    break;
-                case ObjectChangeKind.ChangeGameObjectStructureHierarchy:
-                    stream.GetChangeGameObjectStructureHierarchyEvent(i,
-                        out ChangeGameObjectStructureHierarchyEventArgs changeGameObjectStructureHierarchy);
-                    GameObject gameObject =
-                        EditorUtility.EntityIdToObject(changeGameObjectStructureHierarchy.instanceId) as GameObject;
-                    Debug.Log($"{type}: {gameObject} in scene {changeGameObjectStructureHierarchy.scene}.");
-                    break;
-                case ObjectChangeKind.ChangeGameObjectStructure:
-                    stream.GetChangeGameObjectStructureEvent(i,
-                        out ChangeGameObjectStructureEventArgs changeGameObjectStructure);
-                    GameObject gameObjectStructure =
-                        EditorUtility.EntityIdToObject(changeGameObjectStructure.instanceId) as GameObject;
-                    Debug.Log($"{type}: {gameObjectStructure} in scene {changeGameObjectStructure.scene}.");
-
-                    // GuidComponent guidComponent = gameObjectStructure.GetComponent<GuidComponent>();
-                    // if (guidComponent)
-                    // {
-                    //     foreach (ComponentGuid componentGuid in guidComponent.componentGUIDs)
-                    //     {
-                    //         if (!componentGuid.cachedComponent)
-                    //         {
-                    //             SetGuidState(gameObjectStructure, componentGuid.Guid,
-                    //                 GuidReferenceMappings.GuidState.Orphaned);
-                    //         }
-                    //     }
-                    // }
-
-                    break;
-                case ObjectChangeKind.ChangeGameObjectParent:
-                    stream.GetChangeGameObjectParentEvent(i,
-                        out ChangeGameObjectParentEventArgs changeGameObjectParent);
-                    GameObject gameObjectChanged =
-                        EditorUtility.EntityIdToObject(changeGameObjectParent.instanceId) as GameObject;
-                    GameObject newParentGo =
-                        EditorUtility.EntityIdToObject(changeGameObjectParent.newParentInstanceId) as GameObject;
-                    GameObject previousParentGo =
-                        EditorUtility.EntityIdToObject(changeGameObjectParent.previousParentInstanceId) as
-                            GameObject;
-                    Debug.Log(
-                        $"{type}: {gameObjectChanged} from {previousParentGo} to {newParentGo} from scene {changeGameObjectParent.previousScene} to scene {changeGameObjectParent.newScene}.");
-                    break;
-                case ObjectChangeKind.ChangeGameObjectOrComponentProperties:
-                    stream.GetChangeGameObjectOrComponentPropertiesEvent(i,
-                        out ChangeGameObjectOrComponentPropertiesEventArgs changeGameObjectOrComponent);
-                    Object goOrComponent = EditorUtility.EntityIdToObject(changeGameObjectOrComponent.instanceId);
-                    if (goOrComponent is GameObject go)
-                    {
-                        Debug.Log(
-                            $"{type}: GameObject {go} change properties in scene {changeGameObjectOrComponent.scene}.");
-                    }
-                    else if (goOrComponent is Component component)
-                    {
-                        Debug.Log(
-                            $"{type}: Component {component} change properties in scene {changeGameObjectOrComponent.scene}.");
-                    }
-
-                    break;
-                case ObjectChangeKind.DestroyGameObjectHierarchy:
-                    stream.GetDestroyGameObjectHierarchyEvent(i,
-                        out DestroyGameObjectHierarchyEventArgs destroyGameObjectHierarchyEvent);
-                    // The destroyed GameObject can not be converted with EditorUtility.InstanceIDToObject as it has already been destroyed.
-                    Debug.Log(GlobalObjectId.GetGlobalObjectIdSlow(destroyGameObjectHierarchyEvent.instanceId));
-                    GameObject destroyParentGo =
-                        EditorUtility.EntityIdToObject(destroyGameObjectHierarchyEvent
-                            .parentInstanceId) as GameObject;
-                    Debug.Log(
-                        $"{type}: {destroyGameObjectHierarchyEvent.instanceId} with parent {destroyParentGo} in scene {destroyGameObjectHierarchyEvent.scene}.");
-                    break;
-                case ObjectChangeKind.CreateAssetObject:
-                    stream.GetCreateAssetObjectEvent(i, out CreateAssetObjectEventArgs createAssetObjectEvent);
-                    Object createdAsset = EditorUtility.EntityIdToObject(createAssetObjectEvent.instanceId);
-                    string createdAssetPath = AssetDatabase.GUIDToAssetPath(createAssetObjectEvent.guid);
-                    Debug.Log($"{type}: {createdAsset} at {createdAssetPath} in scene {createAssetObjectEvent.scene}.");
-                    break;
-                case ObjectChangeKind.DestroyAssetObject:
-                    stream.GetDestroyAssetObjectEvent(i, out DestroyAssetObjectEventArgs destroyAssetObjectEvent);
-                    // The destroyed asset can not be converted with EditorUtility.InstanceIDToObject as it has already been destroyed.
-                    Debug.Log(
-                        $"{type}: Instance Id {destroyAssetObjectEvent.instanceId} with Guid {destroyAssetObjectEvent.guid} in scene {destroyAssetObjectEvent.scene}.");
-                    break;
-                case ObjectChangeKind.ChangeAssetObjectProperties:
-                    stream.GetChangeAssetObjectPropertiesEvent(i,
-                        out ChangeAssetObjectPropertiesEventArgs changeAssetObjectPropertiesEvent);
-                    Object changeAsset = EditorUtility.EntityIdToObject(changeAssetObjectPropertiesEvent.instanceId);
-                    string changeAssetPath = AssetDatabase.GUIDToAssetPath(changeAssetObjectPropertiesEvent.guid);
-                    Debug.Log(
-                        $"{type}: {changeAsset} at {changeAssetPath} in scene {changeAssetObjectPropertiesEvent.scene}.");
-                    break;
-                case ObjectChangeKind.UpdatePrefabInstances:
-                    stream.GetUpdatePrefabInstancesEvent(i,
-                        out UpdatePrefabInstancesEventArgs updatePrefabInstancesEvent);
-                    string s = "";
-                    s +=
-                        $"{type}: scene {updatePrefabInstancesEvent.scene}. Instances ({updatePrefabInstancesEvent.instanceIds.Length}):\n";
-                    foreach (int prefabId in updatePrefabInstancesEvent.instanceIds)
-                    {
-                        s += EditorUtility.EntityIdToObject(prefabId) + "\n";
-                    }
-
-                    Debug.Log(s);
-                    break;
-                case ObjectChangeKind.ChangeChildrenOrder:
-                    stream.GetChangeChildrenOrderEvent(i,
-                        out ChangeChildrenOrderEventArgs changeChildrenOrderEventArgs);
-                    Object changeChildren = EditorUtility.EntityIdToObject(changeChildrenOrderEventArgs.instanceId);
-                    Debug.Log(
-                        $"{type}: {changeChildren} in scene {changeChildrenOrderEventArgs.scene}.");
-                    break;
-                case ObjectChangeKind.ChangeRootOrder:
-                    stream.GetChangeRootOrderEvent(i,
-                        out ChangeRootOrderEventArgs changeRootOrderEventArgs);
-                    Object changeRoot = EditorUtility.EntityIdToObject(changeRootOrderEventArgs.instanceId);
-                    Debug.Log(
-                        $"{type}: {changeRoot} in scene {changeRootOrderEventArgs.scene}.");
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
         }
     }
 }
